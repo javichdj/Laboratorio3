7d7d1e76e5881a2869bf1b162dbd35fcaba3fbbb
JazminVS <jazminv8.95@gmail.com>
2016-05-05 10:04:09 -0500

    8klmo
    8kmom

---
+++ Ejercicio1.py
@@ -0,0 +1,28 @@
+a= int( input("Ingrese el primer numero: "))
+b=int(input("Ingrese el segundo numero: "))
+def suma ():
+ 	suma= a+b 
+ 	return suma
+mas= suma()
+print (mas) 
+
+def resta ():
+	resta=a-b
+	return resta
+menos=resta ()
+print (menos)
+
+def division():
+	if b>0:
+		dividir=a/b
+		return dividir
+	else:
+		return ("La division para cero no existe")
+para= division()
+print (para)
+
+	
+
+
+
+
